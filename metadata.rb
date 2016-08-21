24ae7268cebe4334548be6799fe6f2da7d0533aa
Steven Cardoso <steven.s.cardoso@gmail.com>
2016-08-21 14:01:57 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer_email 'patrick.ayoup@gmail.com'
 license          'MIT'
 description      'Installs/Configures virtualenv'
 long_description 'Installs/Configures virtualenv'
-supports         'centos', "= 6.5"
 version          '0.1.1'
 
 depends 'python', '~> 1.4.6'
